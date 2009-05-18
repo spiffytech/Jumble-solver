4d7259271e8c5ee86322cefbfbdcc77ff05035a9
Brian Cottingham <brian@spiffytop.spiffynet>
2009-05-18 10:47:57 -0400

    Moved execution logic from untracked test.py into jumble.py

--- jumble.py
+++ jumble.py
@@ -1,16 +1,60 @@
-def findWord(jumble, list):
-    '''Returns a list of words with all of the same letters as the original word'''
-    newList = []
-    for word in list:
-        if len(jumble) != len(word):
-            continue
+import cPickle as pickle
+
+def main():
+    dictFile = file("words")
+    dict = pickle.load(dictFile)
+    jumbles = []
+    for i in range(0, 5):
+        j = raw_input("Next jumbled word: ")
+        if j == "":
+            break
+
+        jumbles.append([])
+        jumbles[i].append(j)
+        jumbles[i].append(findWord(jumbles[i][0], dict))
+
+    for word in jumbles:
+        print word[1]
+
+
+class jumble:
+    original = ""
+    agrams = []
+    positions = []
+
+
+
+def solve(jumbles):
+    dictFile = file("words")
+    dict = pickle.load(dictFile)
+
+    words = []
+    for j in jumbles:
+        words.append()
+
+
+
+
+def findWord(jumble, words):
+    '''Returns a words of words with all of the same letters as the original word'''
+    newwords = []
+    for word in words:
         isOK = False  # Did the current word pass the test?
+        if len(jumble) != len(word):  # Only single words for now
+            continue
+
         for letter in jumble:
             if not jumble.count(letter) == word.count(letter):
                 break
             if letter == jumble[len(jumble)-1]:
                 isOK = True
+
         if isOK == True:
-            newList.append(word)
+            newwords.append(word)
         isOK = False
-    return newList
+
+    return newwords
+
+
+if __name__ == "__main__":
+    main()
